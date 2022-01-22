9df4452edc51e385e079a97b3d5339caa88dcf0d
Eka Febriansah <febriye12@gmail.com>
2022-01-23 06:51:37 +0700

    Portfolio

--- resources/views/user/products/index.blade.php
+++ resources/views/user/products/index.blade.php
@@ -180,15 +180,30 @@
                 </p>
             </div>
             <hr>
-            <div class="col-md-12">
-                <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Rate This Product
-                </button>
-                <a href="{{ url('add-review/'.$product->slug.'/userreview' ) }}" class="btn btn-outline-primary">
-                   Write a Product
-                </a>
+            <div class="row">
+                <div class="col-md-4">
+                    <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Rate This Product
+                    </button>
+                    <a href="{{ url('add-review/'.$product->slug.'/userreview' ) }}" class="btn btn-outline-primary">
+                       Write a Product
+                    </a>
+                </div>
+                <div class="col-md-8">
+                    <label for="">Om Prakash</label>
+                    <a href="">Edit</a>
+                    <br>
+                    <i class="fas fa-star checked"></i>
+                    <i class="fas fa-star checked"></i>
+                    <i class="fas fa-star checked"></i>
+                    <i class="fas fa-star checked"></i>
+                    <i class="fas fa-star"></i>
+                    <small>Review on 22 Agustus 2022</small>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </div>
             </div>
-
         </div>
     </section><!-- End Portfolio Details Section -->
 
